0244baa76f230274c54a3bd83050f620f625b98d
Ralph Nicholson <nicholson.re@gmail.com>
2023-05-03 15:14:49 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  receive word
+  iterate through each letter of the word
+  if iteration is the same with the letters starting from beginning of word as 
+  it is as starting from the end of the word 
+  then 'word' is a palindrome
 */
 
 /*
   Add written explanation of your solution here
+  receive the string and use the split method to split the 'word' into an array
+  then use reverse() to reverse the array and then use join() to return the array
+  to a string.
+  check if word === to reversedWord, will return true or false
 */
 
 // You can run `node index.js` to view these console logs
